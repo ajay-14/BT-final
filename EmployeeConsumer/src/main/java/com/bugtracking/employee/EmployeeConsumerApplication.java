2914e5d308df70c93238d23eb2b359efc7f63fe4
ajay-14 <59205855+ajay-14@users.noreply.github.com>
2022-01-18 14:47:59 +0530

    Add files via upload

--- EmployeeConsumer/src/main/java/com/bugtracking/employee/EmployeeConsumerApplication.java
+++ EmployeeConsumer/src/main/java/com/bugtracking/employee/EmployeeConsumerApplication.java
@@ -13,17 +13,19 @@ import springfox.documentation.service.Contact;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
-@EnableSwagger2
+
 @SpringBootApplication
+@EnableSwagger2
 public class EmployeeConsumerApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(EmployeeConsumerApplication.class, args);
 	}
+
 	@Bean
 	public Docket swaggerConfiguration() {
 		return new Docket(DocumentationType.SWAGGER_2).select().paths(PathSelectors.any())
-				.apis(RequestHandlerSelectors.basePackage("com.bugtracking.admin.controller")).build()
+				.apis(RequestHandlerSelectors.basePackage("com.bugtracking.employee.controller")).build()
 				.apiInfo(new ApiInfo("SPRING WITH SWAGGER API", "API CREATION", "1.0", "Free to Use",
 						new Contact("Ajay14", "/api", "ajarvnd07@gmail.com"), "API licence", "/api",
 						Collections.emptyList()));
